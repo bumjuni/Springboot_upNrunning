6a01e9f6169eaed8021b75a0f52044b1f2a17e30
JJy <darlian0889@gmail.com>
2025-07-31 16:05:17 +0900

    Feat: GET메서드 추가

--- sbur-rest-demo/src/main/java/com/spring_practice/sbur_rest_demo/SburRestDemoApplication.java
+++ sbur-rest-demo/src/main/java/com/spring_practice/sbur_rest_demo/SburRestDemoApplication.java
@@ -37,3 +37,37 @@ class Coffee {
 		this.name = name;
 	}
 }
+
+@RestController
+@RequestMapping("/")
+class RestApiDemoCotroller {
+	private List<Coffee> coffees = new ArrayList<>();
+
+	public RestApiDemoCotroller() {
+		coffess.addAll(List.of(
+				new Coffee("Cafe Cereza"),
+				new Coffee("Cafe Ganador"),
+				new Coffee("Cafe Lareno"),
+				new Coffee("Cafe Tres Pontas")
+		));
+	}
+
+//	@RequestMapping(value = '/coffees', method = RequestMethod.GET)
+//	Iterable<Coffee> getCoffees() {
+//		return coffees;
+//	}
+	@GetMapping("/coffees")
+	Iterable<Coffee> getCoffees() {
+		return coffees;
+	}
+
+	@GetMapping("/coffees/{id}")
+	Optional<Coffee> getCoffeeById(@PathVariable String id) {
+		for (Coffee c: coffees) {
+			if (c.getId().equals(id)) {
+				return Optional.of(c);
+			}
+		}
+		return Optional.empty();
+	}
+}
