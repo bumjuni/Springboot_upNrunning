416264e6c53f581618e23b36442e26e4cc910ea9
JJy <darlian0889@gmail.com>
2025-07-31 17:20:57 +0900

    Feat: Put, Post, Delete 메서드 추가

--- sbur-rest-demo/src/main/java/com/spring_practice/sbur_rest_demo/SburRestDemoApplication.java
+++ sbur-rest-demo/src/main/java/com/spring_practice/sbur_rest_demo/SburRestDemoApplication.java
@@ -70,4 +70,28 @@ class RestApiDemoCotroller {
 		}
 		return Optional.empty();
 	}
+
+	@PostMapping("/coffees")
+	Coffee postCoffee(@RequestBody Coffee coffee) {
+		coffees.add(coffee);
+		return coffee;
+	}
+
+	@PutMapping("/coffees/{id}")
+	Coffee putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
+		int coffeeIndex = -1;
+
+		for (Coffee c: coffees) {
+			if (c.getId().equals(id)) {
+				coffeeIndex = coffees.indexOf(c);
+				coffees.set(coffeeIndex, coffee);
+			}
+		}
+		return (coffeeIndex == -1) ? postCoffee(coffee) : coffee;
+	}
+
+	@DeleteMapping("/coffees/{id}")
+	Coffee deleteCoffee(@PathVariable String id) {
+		coffees.removeIf(c -> c.getId().equals(id));
+	}
 }
